fb002e4c61cbe3616d522c93d62bd5199260fb63
Sacrezar <thomas.soulas@viacesi.fr>
2020-10-04 11:46:14 +0200

    close #15

--- js/content-moodle.js
+++ js/content-moodle.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector(".site-logo").src = chrome.runtime.getURL("icon/exia_icon_48.png");
+    document.querySelector(".iconlarge").src = chrome.runtime.getURL("icon/conversation.png");
 })
+ 
